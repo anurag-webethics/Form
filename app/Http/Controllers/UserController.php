d37f0af6fe3345732b0b72b9da7abea549c26fe1
anurag-webethics <anurag.webethics@gmail.com>
2024-04-11 16:02:40 +0530

    laravel validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
+use App\Rules\UseValidate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Closure;
@@ -11,12 +12,16 @@ use Closure;
 class UserController extends Controller
 {
     public function addUser(Request $req){
+        // $validate = $req->validate([
+        //     'username'=>['required',function(string $attribute, mixed $value, Closure $fail){
+        //         if(strtoupper('username') !==$value){
+        //             $fail('The :attribute must be uppercase.');
+        //         }
+        //     }],
+        // ]);
+
         $validate = $req->validate([
-            'username'=>['required',function(string $attribute, mixed $value, Closure $fail){
-                if(strtoupper('username') !==$value){
-                    $fail('The :attribute must be uppercase.');
-                }
-            }],
+            'username'=>['required',new UseValidate],
         ]);
         
         return $req->all();
